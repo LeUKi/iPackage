ee6edd3ade5d60f58c56ecdb005fd64b7c2c985a
lafish <1409286026@qq.com>
2020-03-22 19:39:44 +0800

    new

--- js.js
+++ js.js
@@ -1,1 +1,25 @@
-$("#AllExs").html("123");
+function findByID(pID) {
+    var getUrl = "http://118.178.125.139:8080/findByExpressOrderID?id=" + pID;
+    $.get(getUrl, function (data, status) {
+        var Content = "";
+        for (x in data.extended.List) {
+            Content += "<div class=\"card\">" + "<div class=\"card-body\">" + data.extended.List[x].id + " | " + data.extended.List[x].sender + "</div></div><br>";
+        }
+        $("#AllExs").html(Content);
+    });
+}
+
+
+$(function () {
+    // $.get("http://118.178.125.139:8080/findAllExpressOrder", function (data, status) {
+    //     var Content = "";
+    //     for (x in data.extended.List) {
+    //         Content += "<div class=\"card\">" + "<div class=\"card-body\">" + data.extended.List[x].id + " | " + data.extended.List[x].sender + "</div></div><br>";
+    //     }
+    //     $("#AllExs").html(Content);
+    // });
+    $('[data-toggle="tooltip"]').tooltip();//信息弹出
+    $("#addr-btn").click(function () {//搜索弹出
+        $("#addr-notes").slideToggle("fast");
+    });
+});
