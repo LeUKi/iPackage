14b3312554fd152e7d6b54bc1f2410521aa5cdb9
lafish <1409286026@qq.com>
2020-03-24 21:59:12 +0800

    new

--- js.js
+++ js.js
@@ -3,7 +3,7 @@ function findByID(pID, innerwhere) {//插入结果
     var getUrl = "http://118.178.125.139:8080/findByExpressOrderID?id=" + pID;
     $.get(getUrl, function (data, status) {
         if (data.extended.ExpressOrder != null) {
-            Pdata = [];
+            var Pdata = [];
             Pdata[0] = data.extended.ExpressOrder.id;
             Pdata[1] = data.extended.ExpressOrder.sender;
             Pdata[2] = data.extended.ExpressOrder.sender_phone;
@@ -25,9 +25,9 @@ function findByID(pID, innerwhere) {//插入结果
                 Pdata[9] + "</h2></span>" +
                 "<br><br><div class=\"card-body\">快递类型：" +
                 Pdata[4] + "<br>快递内容：" +
-                Pdata[3] + "<br>收件地址：<a href=\"#\" data-toggle=\"tooltip\" title=\"地址编号：" +
+                Pdata[3] + "<br>收件地址：<span data-toggle=\"tooltip\" title=\"地址编号：" +
                 Pdata[6] + "\">" +
-                Pdata[7] + "</a></div></div><div class=\"card-footer\">" +
+                Pdata[7] + "</span></div></div><div class=\"card-footer\">" +
                 Pdata[5] + "<div class=\"btn-group-sm fR\"><button onclick=\"addPg(" + Pdata[0] + ',' + Pdata[6] + ")\" type=\"button\" class=\"btn btn-sm btn-success fa fa-archive\"> 添加到我的快递</button></div></div></div></div>";
         }
         innerwhere.html(Content);
@@ -44,14 +44,47 @@ function addPg(id, addrid) {
     } else {
         temp1.unshift(id);
         localStorage.myPackagesid = JSON.stringify(temp1);
-        var temp2 = JSON.parse(localStorage.addrid);
-        if (!temp2.includes(addrid)) {
-            temp2.unshift(addrid);
-            localStorage.addrid = JSON.stringify(temp2);
+    }
+    var temp2 = JSON.parse(localStorage.addrid);
+    if (!temp2.includes(addrid)) {
+        temp2.unshift(addrid);
+        localStorage.addrid = JSON.stringify(temp2);
+    }
+}
+
+function showAddr() {//显示‍地址簿
+    $("#addrsult").html("");
+    var addrid = JSON.parse(localStorage.addrid);
+    if (addrid.length != 0) {
+        for (x in addrid) {
+            var getUrl = "http://118.178.125.139:8080/findByAdressID?id=" + addrid[x];
+            $.get(getUrl, function (data, status) {
+                var Adata = [];
+                Adata[0] = data.extended.Adress.id;
+                Adata[1] = data.extended.Adress.adr;
+                Adata[2] = data.extended.Adress.phone;
+                Adata[3] = data.extended.Adress.recipient;
+                var addrlist = "<br><div class=\"card\" id=\"addr-notes\"><div class=\"card-body\"><div class=\"input-group \"><input type=\"text\" class=\"form-control\" placeholder=\"" +
+                    Adata[3] + "\" disabled><input type=\"text\" class=\"form-control\" placeholder=\"" +
+                    Adata[2] + "\" disabled><div class=\"input-group-append\"><button data-toggle=\"modal\" data-target=\"#myModal\" chaddr=\"" + Adata[0] + "\" type=\"button\" class=\"btn btn-sm btn-primary fR fa fa-pencil\"></button></div></div><div class=\"input-group mb-2\"><input type=\"text\" class=\"form-control\" placeholder=\"" +
+                    Adata[1] + "\" disabled><div class=\"input-group-append\"><button deladdr=\"" + Adata[0] + " type=\"button\" class=\"btn btn-sm btn-danger fR fa fa-trash-o\"></button></div></div></div></div>";
+                $("#addrsult").append(addrlist);
+                lisenChaddr();
+            })
         }
     }
 }
 
+function lisenChaddr() {
+    $("[deladdr]").unbind();
+    $("[deladdr]").click(function (e) {
+        var temp1 = JSON.parse(localStorage.addrid);
+        var temp2 = temp1.indexOf($(this).attr("chaddr"));
+        temp1.splice(temp2, 1);
+        localStorage.addrid = JSON.stringify(temp1);
+        showAddr();
+    });
+}
 
 function updat() {
     $(".disP-btn").unbind();
@@ -63,9 +96,9 @@ function updat() {
 
 function showmyPg() {
     var Pgidlist = JSON.parse(localStorage.myPackagesid);
-    $("#historyPage").html("<div class=\'card\'><div class=\"card-body\">你还没有快递哦！<br>你可以点击 <button type=\"button\" class=\"btn btn-sm bg-primary fa fa-search srch-btn\" style=\"color: white\"> 添加快递</button> 以搜索或添加你的快递。</div></divc>");
+    $("#histsult").html("<div class=\'card\'><div class=\"card-body\">你还没有快递哦！<br>你可以点击 <button type=\"button\" class=\"btn btn-sm bg-primary fa fa-search srch-btn\" style=\"color: white\"> 添加快递</button> 以搜索或添加你的快递。</div></divc>");
     if (Pgidlist.length != 0) {
-        $("#historyPage").html("");
+        $("#histsult").html("");
         for (x in Pgidlist) {
             var getUrl = "http://118.178.125.139:8080/findByExpressOrderID?id=" + Pgidlist[x];
             $.get(getUrl, function (data, status) {
@@ -91,11 +124,11 @@ function showmyPg() {
                     Pdata[9] + "</h2></span>" +
                     "<br><br><div class=\"card-body\">快递类型：" +
                     Pdata[4] + "<br>快递内容：" +
-                    Pdata[3] + "<br>收件地址：<a href=\"#\" data-toggle=\"tooltip\" title=\"地址编号：" +
+                    Pdata[3] + "<br>收件地址：<span data-toggle=\"tooltip\" title=\"地址编号：" +
                     Pdata[6] + "\">" +
-                    Pdata[7] + "</a></div></div><div class=\"card-footer\">" +
-                    Pdata[5] + "<div class=\"btn-group-sm fR\"><button type=\"button\" class=\"btn btn-sm btn-primary fa fa-pencil\" data-toggle=\"modal\" data-target=\"#myModal\"></button><button type=\"button\" class=\"btn btn-sm btn-danger fa fa-trash-o\"></button></div></div></div></div>";
-                $("#historyPage").append(Content);
+                    Pdata[7] + "</span></div></div><div class=\"card-footer\">" +
+                    Pdata[5] + "<div class=\"btn-group-sm fR\"><button type=\"button\" class=\"btn btn-sm btn-primary fa fa-pencil\" data-toggle=\"modal\" data-target=\"#myModal\"></button><button type=\"button\" class=\"btn btn-sm btn-danger fa fa-trash-o\" data-toggle=\"tooltip\" title=\"这将从服务器中删除\"></button></div></div></div></div>";
+                $("#histsult").append(Content);
                 updat()
             });
         }
@@ -115,6 +148,7 @@ $(function () {
         $("#searchPage").slideUp("fast");
         $("#historyPage").slideUp("fast");
         $("#addrPage").slideDown("fast");
+        // showAddr();
     });
     $(".hisy-btn").click(function () {//点击历史
         $("#searchPage").slideUp("fast");
